d17bcbfde1f984877b743759b56c7be1db8b700a
MerzlyakovaMarya <162551123+MerzlyakovaMarya@users.noreply.github.com>
2025-05-26 02:41:08 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/movies-vite">
-      <App />
-    </BrowserRouter>
+  <BrowserRouter> {/* Убрали basename */}
+  <App />
+</BrowserRouter>
   </React.StrictMode>
 )
