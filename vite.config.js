f49fa37110f2d914143c7ad0d0b226d38f47cd03
MerzlyakovaMarya <162551123+MerzlyakovaMarya@users.noreply.github.com>
2025-05-26 03:34:23 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Убрали /movies-vite/
+  base: '/movies-vite/', // Для GitHub Pages
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
